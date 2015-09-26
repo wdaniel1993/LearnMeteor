addb9bc9f61eb1ce9e913371c39718d83b3ebde0
Daniel <daniel.wallner@mbit.at>
2015-09-26 23:55:43 +0200

    working on seed project

--- client/directives/dwNavigationDirective.js
+++ client/directives/dwNavigationDirective.js
@@ -4,14 +4,20 @@
 angular.module("kiwi-admin").directive("dwNavigation",[function(){
     return {
         restrict: 'E',
-        templateUrl: 'client/templates/navigation.ng.html',
+        replace: true,
+        templateUrl: function(elem,attrs) {
+            return 'client/templates/navigation-' + (attrs.dwType ? attrs.dwType : 'menubar') +'.ng.html'
+        },
         controllerAs: 'navigation',
-        controller:  ['$scope','$mdMenu','PageSettings',function($scope,$mdMenu,PageSettings) {
+        controller:  ['$scope','$mdMenu','PageSettings','$state',function($scope,$mdMenu,PageSettings,$state) {
             this.navigation = PageSettings.navigation();
             PageSettings.subscribeNavigation($scope,function(){
                 this.navigation = PageSettings.navigation();
             });
             this.hide = $mdMenu.hide;
+            this.gotoState = function(state){
+                $state.go(state);
+            }
         }]
     }
 }]);
