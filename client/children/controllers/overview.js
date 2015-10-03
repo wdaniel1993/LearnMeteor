f8be4a26ada6b51be693bc706e24f00bb5fd1fe5
Daniel <daniel.wallner@mbit.at>
2015-10-03 15:46:02 +0200

    working on seed project

--- client/children/controllers/overview.js
+++ client/children/controllers/overview.js
@@ -1,9 +1,22 @@
 /**
  * Created by Daniel on 23.09.2015.
  */
-angular.module("kiwi-admin").controller("OverviewCtrl", ['$meteor', '$state',
-    function($meteor,$state){
-        var ov = this;
-        ov.name = "Daniel";
+angular.module("kiwi-admin").controller("OverviewCtrl", ['$meteor','$meteorCollection', '$state','Organisations','OrganisationManager',
+    function($meteor,$meteorCollection,$state, Organisations,OrganisationManager){
+        var vm = this;
+        vm.organisations = $meteorCollection(Organisations.collection).subscribe('organisations');
+        vm.addOrganisation = function () {
+            var organisation = {
+                title : vm.newOrganisation
+            };
+
+            OrganisationManager.addOrganisation(organisation).then(function(){
+                vm.newOrganisation = '';
+            }, function(error) {
+                vm.error = error;
+            });
+
+            vm.newOrganisation = '';
+        };
     }
 ]);
--- server/services/data/organisations.js
+++ server/services/data/organisations.js
@@ -1,8 +1,8 @@
 /**
  * Created by Daniel on 01.10.2015.
  */
-angular.module("kiwi-admin-server").service('OrganisationManager',['Organisations',function(Organisations) {
-    this.addTodo = function (organisation) {
+angular.module("kiwi-admin-server").service('OrganisationManager',['Organisations',function(Organisations){
+    this.addOrganisation = function (organisation) {
         if (organisation.title.length < 3) {
             throw new Meteor.Error(500, undefined, 'Title must be longer than 3 letters');
         }
@@ -12,8 +12,8 @@ angular.module("kiwi-admin-server").service('OrganisationManager',['Organisation
     };
 
     Meteor.publish('organisations', function () {
-        return Todos.collection.find({});
+        return Organisations.collection.find({});
     });
-}]).config(['ServerAPIProvider',function (ServerAPIProvider) {
+}]).config(['ServerAPIProvider',function(ServerAPIProvider) {
     ServerAPIProvider.register('OrganisationManager');
 }]);
